74f44c0c87fdf0a5d710a4c864e0fe12ba7655d7
Omar Adra <adra.omar2@gmail.com>
2026-04-18 03:19:39 -0300

    Modificado para obtener usuario desde base de datos

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,4 +10,26 @@ const getUsers = async (req, res) => {
     }
 }
 
-export { getUsers };
+const getUser = async (req, res) => {
+    try {
+        const id = req.user.id;
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "ID inválido" });
+        }
+
+        const userId = parseInt(id, 10);
+
+        const [rows] = await pool.query("SELECT id, username, email, lastname, firstname FROM users WHERE id = ?", [userId]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
+};
+
+export { getUsers, getUser };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
-import { getUsers} from "../controllers/user.controller.js";
+import { getUser, getUsers} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get("/", verifyToken, getUsers); // http://localhost:3000/users/
+router.get("/user", verifyToken, getUser); // http://localhost:3000/users/user/
 
 export default router;
